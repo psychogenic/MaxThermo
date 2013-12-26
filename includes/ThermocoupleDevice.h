3e82dfe4e80ab1358925db09f7fdbca65dc50f64
Pat Deegan <pat.github@psychogenic.net>
2013-12-26 15:39:48 -0500

    Added a bit of API documentation to the ThermocoupleDevice base class.

--- includes/ThermocoupleDevice.h
+++ includes/ThermocoupleDevice.h
@@ -82,49 +82,109 @@ namespace MaxThermo
 #endif
 		virtual ~ThermocoupleDevice() {}
 
+		/*
+		 * begin([selectPin])
+		 *
+		 * Call begin() prior to using the device, so we can set everything up.
+		 */
 		virtual void begin(int8_t selectPin=-1);
 
+		/*
+		 * autoInitSPI()/setAutoInitSPI()
+		 * Will initialize SPI on begin(), if true (default).
+		 */
 		bool autoInitSPI() { return auto_init_spi;}
 		void setAutoInitSPI(bool setTo) { auto_init_spi = setTo;}
 
+		/*
+		 * quickReads()/setQuickReads()
+		 * For devices that support it (like the Max31855) quickreads will
+		 * only fetch the thermocouple-related data, and stop short of collecting
+		 * extra bytes.
+		 */
 		bool quickReads() { return quick_reads;}
 		void setQuickReads(bool setTo) { quick_reads = setTo;}
 
 
+		/*
+		 * update() performs an actual read of the thermocouple and caches the result.
+		 */
 		virtual Status::Result update() = 0;
 
+		/*
+		 * Logging related.
+		 * When you startLoggingTo(), all update()s will be stored in the sample_container array.
+		 *
+		 * Call stopLogging() when you are done, or wait for update() to return a
+		 * 	MaxThermo::Status::LogFull
+		 * result (and then call stopLogging()/do whatever you wish with the sample list).
+		 *
+		 * See the TempLogger example.
+		 */
 		bool isLogging() { return sample_log_array != NULL;  }
 		Status::Result startLoggingTo(SampleRawDataInit * sample_container, uint16_t container_size);
 		void stopLogging();
 		LogSummary logSummary(SampleRawDataInit * sample_container, uint16_t container_size);
 
+
+		/*
+		 * status() returns the cached MaxThermo::Status::Result from the last update().
+		 */
 		Status::Result status() { return last_status;}
+
+		/*
+		 * isOk()/isFault()/circuitOpen()/ciruitShort() -- convenience functions for checking the
+		 * status().
+		 */
 		bool isOK() { return last_status == Status::OK;}
 		bool isFault() { return (last_status != Status::OK) && (last_status != Status::NoReading);}
 		bool circuitOpen() { return last_status == Status::OpenCircuit;}
 		bool circuitShort() { return (last_status == Status::ShortToGround) || (last_status == Status::ShortToVcc);}
 
 
+		/*
+		 * tempXXX
+		 * Shorthand methods for the thermocouple temperature data from the last update().
+		 */
 
 		inline bool tempIsNegative() { return thermoTempIsNegative();}
 		inline double temp() { return thermoTemp();}
 		inline int16_t tempAsInt() { return thermoTempAsInt();}
 
+		/*
+		 * thermoTempIsNegative()
+		 * Return true if the temperature is subzero (celcius)
+		 */
 		virtual bool thermoTempIsNegative() = 0;
+
+		/*
+		 * thermoTempAsInt()
+		 * return the thermocouple temperature from the last update(), but ignores the
+		 * digits and is more efficient (if you don't care about the fractional value).
+		 */
 		virtual int16_t thermoTempAsInt() = 0;
+
+		/*
+		 * thermoTemp()
+		 * Returns the (real) thermocouple temperature from the last update().
+		 */
 		virtual double  thermoTemp() = 0;
 
+		/*
+		 * internalTempXXX versions are also available in the implementation, but don't have
+		 * the overhead of virtual methods.
+		 */
 
-
+		/*
+		 * initSPI()
+		 * Used internally to initialize the SPI, but available publically if you wanit.
+		 */
 		virtual void initSPI();
 	protected:
 
 		virtual Status::Result determineError() = 0;
 
 		virtual SampleSummary getSampleSummary(SampleRawDataInit* sample_container, uint16_t index) = 0;
-
-
-
 
 		PinNumber nSelect_pin;
 		PinNumber clock_pin;
